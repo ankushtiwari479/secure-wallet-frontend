2539d7e8a7b69f9602807745613b0fcf39bf4901
ankush tiwari <ankushtiwari@ankushs-MacBook-Pro.local>
2023-10-16 00:55:07 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage/HomePage';
+import axios from 'axios'
 import TransactionsPage from './TransactionsPage/TransactionsPage';
 import Navbar from './NavBar/NavBar';
 import './index.css'
 import './App.css'
 
 function App() {
+  useEffect(()=>{
+    axios.defaults.baseURL = 'https://secure-wallet-2e423483e107.herokuapp.com'; 
+  },[])
   return (
     <div>
       <Navbar/>
